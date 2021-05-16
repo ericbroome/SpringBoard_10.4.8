3d0f02128bdf7ec6ddf7fb33d1543122d7edf29c
eric.s.w.broome.secure@gmail.com <eric.s.w.broome.secure@gmail.com>
2021-05-16 10:29:43 -0500

    Second Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+cont filterOutOdds = (...args) => args.filter(arg => arg % 2 ===0)
+
+function findMin(...args) {
+
+}
+
+
+const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2});
+
+
+/** remove a random element in the items array
+and return a new array without that item. */
+
+function removeRandom(items) {
+
+}
+
+/** Return a new array with every item in array1 and array2. */
+
+function extend(array1, array2) {
+
+}
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+function addKeyVal(obj, key, val) {
+
+}
+
+
+/** Return a new object with a key removed. */
+
+function removeKey(obj, key) {
+
+}
+
+
+/** Combine two objects and return a new object. */
+
+function combine(obj1, obj2) {
+
+}
+
+
+/** Return a new object with a modified key and value. */
+
+function update(obj, key, val) {
+
+}
